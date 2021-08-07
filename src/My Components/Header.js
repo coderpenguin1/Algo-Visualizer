f00f1e7a2deecdfef0dc230b1dc7557bf1da4770
Dhruvjain07 <jain00dhruv@gmail.com>
2021-08-07 20:18:46 +0530

    Update Header.js

--- src/My Components/Header.js
+++ src/My Components/Header.js
@@ -27,7 +27,6 @@ export default function Header() {
                         <li className="nav-item">
                             <a className="nav-link" href="#">Selection Sort</a>
                         </li>
-                            
                     </ul>
                         <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
