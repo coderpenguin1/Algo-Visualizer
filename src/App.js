94ea1a6437ed407e3580aef58e8d5645408fc64a
coderpenguin1 <pengiwor@gmail.com>
2021-08-08 11:48:24 +0530

    added body.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './My Components/Header'
+import Body from './My Components/Body'
 
 function App() {
   return (
     <>
     <Header/>
+    <Body/> 
     </>
   );
 }
---
+++ src/My Components/Body.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {
+      array,
+      currentBubbleTwo,
+      currentQuickTwo,
+      pivot,
+      currentSwappers,
+      currentHeapThree,
+      currentSorted,
+      currentMergeX,
+    } = this.props;
+
+    return (
+      <> <h1> no </h1> </> 
+    )
+  }
+}
+
+export default Body;
